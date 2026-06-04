ba2e58d4a181a462c085a01ec32087aebef6fc84
sinkboy-chen <129042729+sinkboy-chen@users.noreply.github.com>
2026-06-04 22:40:10 +0800

    test mpi to 64 process instead of 128, which is over system limit

--- run_benchmark_mpi.sh
+++ run_benchmark_mpi.sh
@@ -13,7 +13,7 @@ rm -f results/results_mpi.csv
 export UCX_TLS=sm,tcp,self
 export UCX_WARN_UNUSED_ENV_VARS=n
 
-for np in 1 2 4 8 16 32 64 128; do
+for np in 1 2 4 8 16 32 64; do
     echo "----------------------------------------"
     echo "Running benchmark with $np MPI processes..."
     mpirun -np $np ./benchmark_mpi
